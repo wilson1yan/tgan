33d0f687505f32774b8a731c18e5f88370afaaa0
Wilson Yan <wilson1.yan@berkeley.edu>
2021-01-28 13:00:21 -0800

    eidts

--- evaluation/calc_inception.py
+++ evaluation/calc_inception.py
@@ -29,13 +29,10 @@ def calc_inception(ys):
 def main():
     parser = argparse.ArgumentParser(description='inception score')
     parser.add_argument('--gpu', '-g', type=int, default=-1)
-    parser.add_argument('--result_dir', type=str)
-    parser.add_argument('--iter', type=int, default=100000)
-    parser.add_argument('--calc_iter', type=int, default=10000)
-    # parser.add_argument('--mean', type=str, default='/mnt/sakura201/mitmul/codes/tgan2_orig/inception/mean2.npz')
     parser.add_argument('--mean', type=str, default='mean2.npz')
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--interpolation', type=str, default='INTER_CUBIC')
+    parser.add_argument('--samples', type=str, required=True)
     args = parser.parse_args()
 
     np.random.seed(args.seed)
@@ -50,31 +47,27 @@ def main():
     c3dmodel = C3DVersion1()
     c3dmodel.to_gpu()
 
-    # load model
-    fsgen, vgen, _ = get_models(args.result_dir, args.iter)
-    if args.gpu >= 0:
-        fsgen.to_gpu()
-        vgen.to_gpu()
     batchsize = 48
 
     mean = np.load(args.mean)['mean'].astype('f')
     mean = mean.reshape((3, 1, 16, 128, 171))[:, :, :, :, 21:21 + 128]
 
+    all_x = np.load(args.samples)
+
     # generator
     ys = []
-    for i in tqdm(range(args.calc_iter // batchsize)):
-        x, _, _ = make_video(fsgen, vgen, batchsize)
-        n, c, f, h, w = x.shape
-        x = x.transpose(0, 2, 3, 4, 1).reshape(n * f, h, w, c)
-        x = x * 128 + 128
+    for i in tqdm(range(0, all_x.shape, batchsize)):
+        x = all_x[i:i + batchsize] # (b, c, t, h, w) in [0, 255]
+        n, c, f, h, w = x.shape 
+        x = x.transpose(0, 2, 3, 4, 1).reshape(n * f, h, w, c) # (b * t, h, w, c)
         x_ = np.zeros((n * f, 128, 128, 3))
         for t in range(n * f):
             x_[t] = np.asarray(
                 cv.resize(x[t], (128, 128), interpolation=args.interpolation))
-        x = x_.transpose(3, 0, 1, 2).reshape(3, n, f, 128, 128)
+        x = x_.transpose(3, 0, 1, 2).reshape(3, n, f, 128, 128) # (b * t, h, w, c) -> (c, b, t, h, w)
         x = x[::-1] - mean  # mean file is BGR-order while model outputs RGB-order
         x = x[:, :, :, 8:8 + 112, 8:8 + 112].astype('f')
-        x = x.transpose(1, 0, 2, 3, 4)
+        x = x.transpose(1, 0, 2, 3, 4) # (c, b, t, h, w) -> (b, c, t, h, w)
         with chainer.using_config('train', False) and \
                 chainer.no_backprop_mode():
             # C3D takes an image with BGR order
@@ -84,9 +77,7 @@ def main():
     ys = np.asarray(ys).reshape((-1, 101))
 
     score = calc_inception(ys)
-    with open('{}/inception_iter-{}_{}.txt'.format(args.result_dir, args.iter, inter_method), 'w') as fp:
-        print(args.result_dir, args.iter, args.calc_iter, args.mean, score, file=fp)
-        print(args.result_dir, args.iter, 'score:{}'.format(score))
+    print('Score:', score)
 
     return 0
 
